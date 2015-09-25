c7643f1686ac80bf8b5c6585b8fd7e73f0ff9809
rmc1579 <rmc1579@gmail.com>
2015-09-24 21:09:32 -0600

    Adding js-array project files

--- arraysPractice.js
+++ arraysPractice.js
@@ -5,8 +5,11 @@ var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
   //Code Here
+  function first(id){
+    document.write(id[0]);
+  }
 
-
+  first(arr);
 //Next problem
 
 
@@ -16,8 +19,11 @@ var arr = [40,50,60];
 
 
   //Code Here
+  function last(id){
+    document.write("<br />" + id.pop());
+  }
 
-
+  last(arr);
 //Next Problem
 
 
@@ -25,8 +31,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
   //Code Here
-
-
+  function looper(id){
+    for (var i=0; i<id.length; i++){
+      document.write("<br />" + id[i]);
+    }
+  }
+  looper(family);
 //Next problem
 
 
@@ -35,7 +45,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
   //Code Here
-
+  function reversedLooper(id){
+    
+    //for (var r = 0; r<id.length; r++){
+      document.write("<br />" + id.reverse());
+    //}
+  }
+  reversedLooper(letters);
 
 //Next Problem
 
@@ -44,12 +60,19 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
   //Code Here
-
+  function evenFinder(id){
+    for (var i = 0; i < id.length; ++i) { 
+      if ((id[i] % 2) === 0) {
+          document.write("<br />" + id[i]);
+      }
+    }
+  }
+  evenFinder(nums);
 
 //Next problem
 
 
-var nums = [1,2,34,54,55,34,32,11,19,17,54,66,13];
+var nums2 = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 var evens = [];
 var odds = [];
 //Write a function called divider that is given three arguments, nums, evens, and odds.
@@ -58,7 +81,27 @@ var odds = [];
 
 
   //Code Here
-
+  function divider(n1,n2,n3){
+    for (var i = 0; i < n1.length; ++i) { 
+      if ((n1[i] % 2) === 0) {
+        evens.push(n1[i]);
+      }
+      else{
+        odds.push(n1[i]);
+      }
+    }
+    document.write("<br />=============<br />=== Evens: ====<br />=============<br />");
+    for (i=0;i<evens.length;i++)
+    {
+      document.write(evens[i] + "<br />");
+    }
+    document.write("<br />=============<br />=== Odds: ====<br />=============<br />");
+    for (i=0;i<odds.length;i++)
+    {
+      document.write(odds[i] + "<br />");
+    }
+  }
+  divider(nums2,evens,odds);
 
 //Next Problem
 
@@ -70,7 +113,20 @@ var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 //Above you're given a function that will return a random number between 0 and 30, and an array full of numbers. Your job is to write a function named finder that will get a random number, then loop through the array to see if that random number is in the array. If it is, return true, if it's not, return false
 
   //Code Here
-
+  function finder(idarray){
+    document.write("<br /><br />");
+    var getrandom = getRandomArbitrary();
+    document.write(getrandom + "<br />");
+    if (idarray.indexOf(getrandom) > -1){
+        document.write("true");        
+        return true;
+    }
+    else{
+      document.write("false");    
+      return false;
+    }
+  }
+  finder(numbers);
 
 //Next problem
 
@@ -80,8 +136,10 @@ var str = 'this is my sentence';
 //Write a function called reverse that takes a given str as it's only argument and returns that string after it's been reversed
 
   //Code Here
-
-
+  function reverse(id){
+    return id.split('').reverse().join('');
+  }
+  document.write ("<br /><br />Reversed sentence: " + reverse(str));
 //Next Problem
 
 
